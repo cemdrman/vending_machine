b07ee768d354f2d913c55dc6d0e1b9af1071996d
Cem Dırman <cemdrman@gmail.com>
2021-05-26 23:21:04 +0300

    sugar addable for hot drink

--- src/main/java/com/bilisimio/model/HotDrink.java
+++ src/main/java/com/bilisimio/model/HotDrink.java
@@ -1,5 +1,11 @@
 package com.bilisimio.model;
 
-public class HotDrink extends Drink{
+public class HotDrink extends Drink implements SugarAddable{
+
+	@Override
+	public void add(int sugarCount) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
---
+++ src/main/java/com/bilisimio/model/SugarAddable.java
@@ -0,0 +1,7 @@
+package com.bilisimio.model;
+
+public interface SugarAddable {
+
+	void add(int sugarCount);
+
+}
