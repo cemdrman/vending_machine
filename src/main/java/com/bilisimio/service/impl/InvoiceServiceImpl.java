64cbdb1133fea2b61d81ec00b8a95bfc2dedeb6d
Cem Dırman <cemdrman@gmail.com>
2021-05-27 16:14:46 +0300

    invoiceServiceImpl

---
+++ src/main/java/com/bilisimio/service/impl/InvoiceServiceImpl.java
@@ -0,0 +1,15 @@
+package com.bilisimio.service.impl;
+
+import com.bilisimio.input.InvoiceRequest;
+import com.bilisimio.output.InvoiceResponse;
+import com.bilisimio.service.InvoiceService;
+
+public class InvoiceServiceImpl implements InvoiceService {
+
+	@Override
+	public InvoiceResponse payInvoice(InvoiceRequest invoiceRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
