d2ab80f92270b757e40adc2e8c6d52afde27f1c0
Cem Dırman <cemdrman@gmail.com>
2021-05-27 16:14:36 +0300

    basketServiceImpl

---
+++ src/main/java/com/bilisimio/service/impl/BasketServiceImpl.java
@@ -0,0 +1,14 @@
+package com.bilisimio.service.impl;
+
+import com.bilisimio.output.ProductResponse;
+import com.bilisimio.service.BasketService;
+
+public class BasketServiceImpl implements BasketService {
+
+	@Override
+	public ProductResponse addProduct(String productId, int count) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
