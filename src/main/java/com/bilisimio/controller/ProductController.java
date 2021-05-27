e9fe9d1415a5268bdc86a36e0ba5a3bb4672047e
Cem Dırman <cemdrman@gmail.com>
2021-05-27 16:45:30 +0300

    modified paths

--- src/main/java/com/bilisimio/controller/ProductController.java
+++ src/main/java/com/bilisimio/controller/ProductController.java
@@ -1,27 +1,26 @@
 package com.bilisimio.controller;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bilisimio.output.ProductResponse;
 import com.bilisimio.service.ProductService;
 
-@RestController(value = "api/products/")
+@RestController
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping
+	@GetMapping("/products")
 	public ProductResponse getAllProducts() {
 		return productService.getAllProducts();
 	}
 
-	@GetMapping(path = "{product_id}")
-	public ProductResponse getProducts(@PathParam(value = "product_id") int productId) {
+	@GetMapping("/products/{productId}")
+	public ProductResponse getProducts(@PathVariable String productId) {
 		return productService.getProduct(productId);
 	}
 
