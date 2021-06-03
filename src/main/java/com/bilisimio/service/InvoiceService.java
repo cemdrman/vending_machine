39e285dbe422ae76bf04c7fca09effa3989243c5
Cem Dırman <cemdrman@gmail.com>
2021-06-04 01:52:11 +0300

    service connection and prepare bill

--- src/main/java/com/bilisimio/service/InvoiceService.java
+++ src/main/java/com/bilisimio/service/InvoiceService.java
@@ -1,13 +1,12 @@
 package com.bilisimio.service;
 
-import org.springframework.http.ResponseEntity;
-
+import com.bilisimio.exception.VendingMachineException;
 import com.bilisimio.input.InvoiceRequest;
 import com.bilisimio.output.InvoiceResponse;
 import com.bilisimio.output.ServiceResponse;
 
 public interface InvoiceService {
 
-	ResponseEntity<ServiceResponse<InvoiceResponse>> payInvoice(InvoiceRequest invoiceRequest);
+	ServiceResponse<InvoiceResponse> payInvoice(InvoiceRequest invoiceRequest) throws VendingMachineException;
 
 }
