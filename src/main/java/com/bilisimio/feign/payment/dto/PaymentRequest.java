cffac732af52ad38fce7b0f145150cead9a9987a
Cem Dırman <cemdrman@gmail.com>
2021-06-04 01:28:09 +0300

    payment dto

--- src/main/java/com/bilisimio/feign/payment/dto/PaymentRequest.java
+++ src/main/java/com/bilisimio/feign/payment/dto/PaymentRequest.java
@@ -1,5 +1,18 @@
 package com.bilisimio.feign.payment.dto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequest {
 
+	private String ccNo;
+	private double price;
+	private LocalDate date;
+	private int cvc;
 }
--- src/main/java/com/bilisimio/feign/payment/dto/PaymentResponse.java
+++ src/main/java/com/bilisimio/feign/payment/dto/PaymentResponse.java
@@ -1,5 +1,18 @@
 package com.bilisimio.feign.payment.dto;
 
-public class PaymentResponse {
+import org.springframework.http.HttpStatus;
+
+import com.bilisimio.output.ServiceResponseHeader;
+
+public class PaymentResponse extends ServiceResponseHeader {
+
+	public PaymentResponse() {
+
+	}
+	
+	public PaymentResponse(HttpStatus statusCode, String errorMessage) {
+		super(statusCode, errorMessage);
+		// TODO Auto-generated constructor stub
+	}
 
 }
