9668e21d95cc3cf3dd27e8781b8176df8b92cecc
Cem Dırman <cemdrman@gmail.com>
2021-06-04 01:50:24 +0300

    added overload methods

--- src/main/java/com/bilisimio/output/ResponseBuilder.java
+++ src/main/java/com/bilisimio/output/ResponseBuilder.java
@@ -8,9 +8,19 @@ public class ResponseBuilder {
 
 	}
 
+	public static <T> ServiceResponse<T> success(T data) {
+		ServiceResponseHeader header = new ServiceResponseHeader(HttpStatus.OK, null);
+		return new ServiceResponse<>(header, data);
+	}
+	
 	public static <T> ServiceResponse<T> success(T data, HttpStatus status) {
 		ServiceResponseHeader header = new ServiceResponseHeader(status, null);
 		return new ServiceResponse<>(header, data);
+	}
+	
+	public static <T> ServiceResponse<T> failure(String errorMessage) {
+		ServiceResponseHeader header = new ServiceResponseHeader(HttpStatus.NOT_FOUND, errorMessage);
+		return new ServiceResponse<>(header, null);
 	}
 
 	public static <T> ServiceResponse<T> failure(String errorMessage, HttpStatus status) {
