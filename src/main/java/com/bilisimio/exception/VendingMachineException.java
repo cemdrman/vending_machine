da9a871fd7427f2e5991c2f8d84824b22c0920b9
Cem Dırman <cemdrman@gmail.com>
2021-06-04 01:27:06 +0300

    custom exception and handler

---
+++ src/main/java/com/bilisimio/exception/VendingMachineException.java
@@ -0,0 +1,9 @@
+package com.bilisimio.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.EXPECTATION_FAILED)
+public class VendingMachineException extends Exception {
+
+}
