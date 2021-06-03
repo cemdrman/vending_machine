945811b1e03dec133d4eb772f77faee358f2c6f4
Cem Dırman <cemdrman@gmail.com>
2021-06-04 01:50:11 +0300

    added cons

--- src/main/java/com/bilisimio/exception/VendingMachineException.java
+++ src/main/java/com/bilisimio/exception/VendingMachineException.java
@@ -3,7 +3,12 @@ package com.bilisimio.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @ResponseStatus(code = HttpStatus.EXPECTATION_FAILED)
 public class VendingMachineException extends Exception {
+	
+	private String errorMessage;
 
 }
